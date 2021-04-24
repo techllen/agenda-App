37d2b8726c4c991186ed6c46d04beda65340c18a
THE-Allen <allenmwita21@gmail.com>
2021-04-24 17:19:42 -0400

    update login

--- agendaapp/src/com/agendaApp/controller/LoginServlet.java
+++ agendaapp/src/com/agendaApp/controller/LoginServlet.java
@@ -1,8 +1,7 @@
-
+package com.agendaApp.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
