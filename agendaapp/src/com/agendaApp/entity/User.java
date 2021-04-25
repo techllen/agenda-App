15a9e47b723142778cd84068ab63c2839b1d4f42
THE-Allen <allenmwita21@gmail.com>
2021-04-25 17:14:11 -0400

    working logindao

--- agendaapp/src/com/agendaApp/entity/User.java
+++ agendaapp/src/com/agendaApp/entity/User.java
@@ -7,14 +7,22 @@ public class User implements Serializable {
      * 
      */
     private static final long serialVersionUID = 1;
-    private String username;
-    private String password;
+    private  String username;
+    private  String password;
+    
+    public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
 
-    public String getUsername() {
+	public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+   
+
+	public void setUsername(String username) {
         this.username = username;
     }
 
--- agendaapp/src/com/agendaApp/model/LoginDao.java
+++ agendaapp/src/com/agendaApp/model/LoginDao.java
@@ -1,34 +1,87 @@
 package com.agendaApp.model;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.*;
+
+import com.agendaApp.entity.User;
 
 public class LoginDao {
+	
+	//JDBC Driver name and database URL
+	final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+	final String DB_URL = "jdbc:mysql://localhost:3306/users_db?useSSL=false";
 
-	public boolean validatemethod(Datasource datasource) {
-		//1.Initialize connection objects
-		List<User> listusers = new ArrayList<>();
-		Connection connect = null;
-		Statement stmt = null;
-		ResultSET rs = null;
+	//Database credentials
+	final String USER = "root";
+	final String PASS = "@Mysql21";
+	
+	boolean status = false;
+
+	public boolean validatelogin(User user) {
+		String enteredusername = user.getUsername();
+		String enteredpassword = user.getPassword();
+		
+		Connection connection = null;
 
 		try {
-			connect = dataSource.getConnection();			
-			//2: Create a SQL statements string
-			String query = "Select * from users";
-			stmt = connect.createStatement();
-			//3: Execute SQL query
-			rs = stmt.executeQuery(query);
-			// Step 4: Process the result set
-			while(rs.next()){
-				rs.getString("username")
-				rs.getString("password")));
-			}	
-		} catch (SQLException e) {
+			//registering JDBC Driver
+
+			Class.forName(JDBC_DRIVER);
+
+			//opening a connection
+			connection = DriverManager.getConnection(DB_URL,USER,PASS);
+
+			//making a parameterized prepared statement
+			String query = "select * from users where username = ? and password = ? ";
+			//String query = "select * from users";
+
+			
+			PreparedStatement preparedStatement= connection.prepareStatement(query);
+
+			preparedStatement.setString(1, enteredusername);
+			preparedStatement.setString(2, enteredpassword);
+			//Execute a query
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			//extract data from resultset
+			status =rs.next();
+			while(rs.next()) {
+				System.out.println(rs.getString("username"));
+			}
+
+			//SClean-up environment
+			System.out.println("matare");
+
+			rs.close();
+			preparedStatement.close();
+			connection.close();
+		}catch(SQLException se){
+			//Handle errors for JDBC
+			se.printStackTrace();
+		}catch(Exception e){
+			//Handle errors for Class.forName
 			e.printStackTrace();
-		}
-		return true;
+		}finally{
+			//finally block used to close resources
+
+			try{
+				if(connection!=null)
+					connection.close();
+			}catch(SQLException se){
+				se.printStackTrace();
+			}//end finally try
+		}//end try
+		return status;
+
+	}
+
+	public static void main(String[] args) {
+		LoginDao login = new LoginDao();
+		User us = new User("matare21", "matare");
+				login.validatelogin(us);
 	}
 
 }
+
+
 
